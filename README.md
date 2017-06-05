36b23cb06c9fb3879834742f0e54f0372d560b42
fastily <fastily@users.noreply.github.com>
2017-06-04 19:53:01 -0700

    initial commit, adding files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# text2hash-toollabs
-converts text to a hash
+# text2hash
+
+Simple javascript application that helps hash text using MD5/SHA1/SHA2.
---
+++ text2hash.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function(){
+
+	var calcHash = function() {
+
+		var md;
+		switch($("#alg-select option:selected").val())
+		{
+			case 'md5':
+				md = forge.md.md5.create();
+				break;
+			case 'sha1':
+				md = forge.md.sha1.create();
+				break;
+			case 'sha-256':
+				md = forge.md.sha256.create();
+				break;
+			case 'sha-384':
+				md = forge.md.sha384.create();
+				break;
+			case 'sha-512':
+				md = forge.md.sha512.create();
+				break;
+			default:
+				alert("Should never reach here?");
+				break;
+		}
+
+		var text = $('#comment').val();
+		if(text === "")
+			$("#hashed").val("");
+		else
+		{
+			md.update(text);
+			$("#hashed").val(md.digest().toHex());
+		}
+	};
+
+
+	$('#comment').keyup(calcHash);
+	$('#alg-select').change(calcHash);
+});
